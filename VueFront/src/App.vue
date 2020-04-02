a32ada9851c36603b498cef60e59d853b083285c
zhengxuan <zhengxuan@hzcominfo.com>
2020-04-02 16:21:00 +0800

    VueFront/src/App.vue添加刷新页面store state存储功能

--- VueFront/src/App.vue
+++ VueFront/src/App.vue
@@ -14,7 +14,15 @@
 <script>
 import PageMenu from '@/components/PageMenu'
 export default {
-  components: { PageMenu }
+  components: { PageMenu },
+  created () {
+    // 存储全局的store到sessionStorage里面解决刷新丢失store数据的问题
+    window.sessionStorage.getItem('GlobalStateInfo') && this.$store.replaceState(Object.assign(this.$store.state, JSON.parse(window.sessionStorage.getItem('GlobalStateInfo'))))
+    // 在页面刷新的时候将vuex里的信息保存到localStorage里
+    window.addEventListener('beforeunload', () => {
+      window.sessionStorage.setItem('GlobalStateInfo', JSON.stringify(this.$store.state))
+    })
+  }
 }
 </script>
 
